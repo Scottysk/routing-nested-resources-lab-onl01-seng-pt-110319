77d4f8b7d71881539f2fc9bd4ed715753653ce19
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-04-27 03:45:52 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @artist = Artist.find(params[:artist_id]).artist
+      @artist = Artist.find(params[:artist_id]).artists
     else 
       @artist = Artist.all
     end
